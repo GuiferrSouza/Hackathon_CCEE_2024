02eea6327e37f26cc80c2a5e939cb642be9daca0
DevCodeMathias <lauramathias03@gmail.com>
2024-04-02 17:03:39 -0300

    config:model

--- src/models/RegisterMigration.js
+++ src/models/RegisterMigration.js
@@ -2,9 +2,19 @@ const db = require("../config/configDb");
 const mongoose = require("mongoose")
 
 
-let DbMigration = new mongoose.Schema({
-    //dados da migração
-//esta na pasta de migração  do postman  
-})
+let DbMigrationSchema = new mongoose.Schema({
+    codigoConectado: { type: Number, required: true },
+    codigoPerfil: { type: Number, required: true },
+    codigoUnidadeConsumidora: { type: Number, required: true },
+    codigoVarejista: { type: Number, required: true },
+    dataDenuncia: { type: Date, required: true },
+    dataInicioContratacao: { type: Date, required: true },
+    tipoConectado: { type: String, enum: ['DISTRIBUIDORA_SUPRIDORA'], required: true },
+    tipoDocumento: { type: String, enum: ['CPF'], required: true },
+    numeroDocumento: { type: String, required: true },
+    linkCCV: { type: String, required: true }
+});
 
-module.exports = DbMigration
+let cadastroMigration= mongoose.model("nome do esquema no banco", DbMigration)
+
+module.exports = cadastroMigration 
